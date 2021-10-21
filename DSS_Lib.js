8d169c4c3874786325afc5a2046630661685c1d4
dryty93y <dryden.inc718@gmail.com>
2021-10-21 18:48:30 -0400

    scroll as json object
    scroll is converted into json object and passed to the dom.

--- DSS_Lib.js
+++ DSS_Lib.js
@@ -89,23 +89,6 @@ const DeadSeaScrolls = {
             }
         },
 
-        genesis_functions: {
 
-
-            write: function (write_this) {
-                console.log(write_this);
-
-            },
-            if_statement: function (condition,exec)  {
-                if (condition) {
-                    for (let operations= 0; operations < exec.length; operations ++ ){
-                        this.write(exec.split(",")[0].toString().split(":")[1])
-
-
-                    }
-                }
-            },
-
-        }
     }
 module.exports ={DeadSeaScrolls};
